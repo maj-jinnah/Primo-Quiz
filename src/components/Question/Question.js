1195422d7ca780c84874c30e9293d08eb8c04962
maj-jinnah <maj.jinnah@gmail.com>
2022-10-12 09:45:57 +0600

    got the question

---
+++ src/components/Question/Question.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Question = ({question}) => {
+    console.log(question);
+    return (
+        <div>
+            <h1>this is question!</h1>
+        </div>
+    );
+};
+
+export default Question;
--- src/components/TopicQuizDetails/TopicQuizDetails.js
+++ src/components/TopicQuizDetails/TopicQuizDetails.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Question from '../Question/Question';
 
 const TopicQuizDetails = () => {
     const topicQuizDetails = useLoaderData();
-    const questionsDetails = topicQuizDetails.data.questions
-    console.log(questionsDetails);
+    const quizName = topicQuizDetails.data.name
+
+    const questions = topicQuizDetails.data.questions
+    // console.log(questions)
 
     return (
         <div>
-            <h1>quiz are here</h1>
+            <h1 className='my-5 text-3xl font-semibold text-violet-500'>Quiz of {quizName}</h1>
+            {
+                questions.map( question => <Question
+                    key={question.id}
+                    question = {question}
+                ></Question>)
+            }
         </div>
     );
 };
